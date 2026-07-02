53527d2865faa280509d590df489e03102520233
ega <eganeue@gmail.com>
2026-07-02 22:32:22 +0700

    Initial Commit

---
+++ urbanan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  urbanan
+//
+//  Created by Muhamad Ega Nugraha on 02/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ urbanan/urbananApp.swift
@@ -0,0 +1,17 @@
+//
+//  urbananApp.swift
+//  urbanan
+//
+//  Created by Muhamad Ega Nugraha on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct urbananApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
