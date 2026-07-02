4f07d6ba266dc0573621cf69fe7d9a35d785ea55
ega <eganeue@gmail.com>
2026-07-02 22:38:08 +0700

    Challenge 4 Repo urbanan

---
+++ urbanan/ViewModels/ContentViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentViewModel.swift
+//  urbanan
+//
+//  Created by Muhamad Ega Nugraha on 02/07/26.
+//
+
+import Foundation
+
+@Observable
+class ContentViewModel {
+    var message: String = "Hello, world!"
+}
--- urbanan/Views/ContentView.swift
+++ urbanan/Views/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = ContentViewModel()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(viewModel.message)
         }
         .padding()
     }
